f80619f3474b6c4dc7fb1c68958571c30f42d4fc
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-02-24 19:55:50 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,29 @@
 {
-  description = "Description for the project";
+  description = "Unionlabs IBC Template";
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    foundry.url = "github:shazow/foundry.nix/monthly";
   };
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, foundry, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ];
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = pkgs.hello;
+      perSystem = { config, self', inputs', system, ... }: {
+        _module.args.pkgs = import inputs'.nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlays.default ];
+        };
       };
+      imports = [
+        ({ ... }: {
+          perSystem = { pkgs, ... }: {
+            packages.default = pkgs.hello;
+            devShells.default =
+              pkgs.mkShell { buildInputs = with pkgs; []; };
+          };
+        })
+      ];
       flake = { };
     };
 }
